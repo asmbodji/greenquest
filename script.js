1feed3d64d4cffaf1a51d5e95b4f9e66574b27bd
asmbodji <164932520+asmbodji@users.noreply.github.com>
2025-05-21 14:18:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var typed = new Typed(".typing",{
+    strings:["Technicien informatique industrielle","Technicien vision industrielle","Dev web"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+
+
+// Add this to your script.js for toggling navigation on small screens
+document.querySelector('.nav-toggler').addEventListener('click', () => {
+    document.querySelector('.aside').classList.toggle('open');
+    document.querySelector('.nav-toggler').classList.toggle('active');
+});
+
+
+
+document.querySelector('.nav-toggler').addEventListener('click', () => {
+    document.querySelector('.aside').classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+});
+
+
+function toggleSearch() {
+    const searchForm = document.querySelector('.search-form');
+    searchForm.style.display = searchForm.style.display === 'block' ? 'none' : 'block';
+}
---
+++ script1.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".type",{
+    strings:["MULTIMEDIA STORE","Votre boutique","de technologie"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
