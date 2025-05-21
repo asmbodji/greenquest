b60009ae7163a7ebb381ab7acefe66d5471862f0
asmbodji <164932520+asmbodji@users.noreply.github.com>
2025-05-21 16:04:33 +0200

    Add files via upload

--- landing.php
+++ landing.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION['user'])){
     header('Location:index.php');
     die();
 }
-$req = $bdd->prepare('SELECT * FROM utilisateurs WHERE token = ?');
+$req = $pdo->prepare('SELECT * FROM utilisateurs WHERE token = ?');
 $req->execute(array($_SESSION['user']));
 $data = $req->fetch();
 ?>
